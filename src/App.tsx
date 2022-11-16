a563ce4934a426b445011b000cd760eaf9f3f13f
Wojciech Domżał <wojciech.domzal03@gmail.com>
2022-11-16 21:16:46 +0100

    Added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route, Link, Router} from "react-router-dom";
+
+import './App.css';
+import {HomePage} from "./pages/HomePage";
 
 export const App = () => (
-  <h1>Init project</h1>
+  <div className="wrapper">
+    <h1>Flashcards @1.0.0</h1>
+    <BrowserRouter>
+    <nav className="nav-main">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+            <button>Add word</button>
+        </li>
+        <li>
+          <Link to="test">Test</Link>
+        </li>
+      </ul>
+    </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+      </Routes>
+    </BrowserRouter>
+  </div>
 );
